9845e8befd90fdd2a6157526e73d22546b595623
McDic <spongbob9876@naver.com>
2017-09-13 00:04:37 +0900

    Removed operatorFunctions again

--- MDMS/MDMS_regex.py
+++ MDMS/MDMS_regex.py
@@ -96,15 +96,6 @@ class reStr:
                             '=':2, '+=':2, '-=':2, '*=':2, '/=':2, '%=':2, '^=':2,
                             'And':2, 'Not':1, 'Or':2, ',':2,
                             '[]':2}
-
-    # [Operator][(type,type..)] = function
-    # function -> mcdic_mapscripter:__system/__operation/function/__sub0
-    operatorFunctions = {'+': {("int", "int"): "__add/__int_int",
-                               ("float", "float"): "__add/__float_float"},
-                         '-': {("int", "int"): "__sub/__int_int",
-                               ("float", "float"): "__sub/__float_float"},
-                         '*': {("int", "int"): "__mul/__int_int" },
-                         }
     
     duplicatedOperatorNames = {"+": {"Default": "+", "Operator":"+@", None:"+@"},
                                "-": {"Default": "-", "Operator":"-@", None:"-@"}
